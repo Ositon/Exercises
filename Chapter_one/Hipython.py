a0bee22784fbb1d70139251f3a7f2c77a12b4703
Mirriam <mt.victoriarodriguez@ugto.mx>
2022-01-17 14:35:02 -0600

    MyFirsttime

---
+++ Chapter_one/Hipython.py
@@ -0,0 +1,26 @@
+#PLAY WITH ME BABY
+
+print('       *      \n      **      \n     ****     \n    ******     \n   ********     ')
+
+def Mydrag():
+    if num==1:
+        print('       *      ')
+    if num==2:
+        print('      **      ')
+    if num==3:
+        print('     ****     ')
+    if num==4:
+        print('    ******     ')
+    if num==5:
+        print('   ********     ')
+
+'''while True:
+    num=int(input('Ingresa una numero entre 1 y 5: \nCero para Salir\n:'))
+    if num==0:
+     break'''
+option=False
+while not option:
+    num=int(input('Ingresa una numero entre 1 y 5: \nCero para Salir\n:'))
+    Mydrag()
+    if num==0:
+        option=True
---
+++ Chapter_two/Strings.py
@@ -0,0 +1,5 @@
+print('Hello again')
+i = 1
+while i < 6:
+  print(i)
+  i += 1
